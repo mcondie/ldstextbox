44c043a0a0d3b88ea24aa4e86cdc362f4ca4d246
Matthew Condie <mattcondie@ldschurch.org>
2015-06-22 10:44:31 -0600

    finished textbox

--- components/lds-textbox/lds-textbox.controller.js
+++ components/lds-textbox/lds-textbox.controller.js
@@ -5,7 +5,26 @@ function LDSTextboxController(){
 	var ldsText = this;
 
 	_.extend(ldsText, {
+		placeholder: ldsText.label,
+		blur: Blur,
+		focus: Focus,
+		showLabel: ShowLabel,
+		isEditing: true
 	});
+
+	function ShowLabel(){
+		return ldsText.model != '' || ldsText.isEditing;
+	}
+
+	function Blur(){
+		ldsText.placeholder = ldsText.label;
+		ldsText.isEditing = false;
+	}
+
+	function Focus(){
+		ldsText.placeholder = '';
+		ldsText.isEditing = true;
+	}
 }
 
 angular.module('ldstextbox')
